8b302011d996fc81e87576e5fe65acc1845fbef4
vuongnqhe170455@fpt.edu.vn <vuongnqhe170455@fpt.edu.vn>
2025-08-16 07:23:08 +0700

    Feat: Implement Mentor List page with pagination

--- src/java/dao/MentorDAO.java
+++ src/java/dao/MentorDAO.java
@@ -100,6 +100,48 @@ public class MentorDAO extends DBContext {
         return list;
     }
 
+     public int getTotalMentor() {
+        query = "SELECT COUNT(*) count FROM Mentor";
+        try {
+            ps = connection.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int x = rs.getInt("count");
+                return x;
+            }
+        } catch (Exception e) {
+        }
+        return 0;
+    }
+     
+     public List<Mentor> pagingMentor(int index) {
+        List<Mentor> list = new ArrayList<>();
+        query = "SELECT * FROM Mentor\n"
+                + "ORDER BY id\n"
+                + "OFFSET ? ROWS FETCH NEXT 3 ROWS ONLY";
+        try {
+            ps = connection.prepareStatement(query);
+            ps.setInt(1, (index - 1) * 3);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                int accountid = rs.getInt("accountid");
+                String mentorname = rs.getString("name");
+                String address = rs.getString("address");
+                String phone = rs.getString("phone");
+                Date birthday = rs.getDate("birthday");
+                String sex = rs.getString("sex");
+                String introduce = rs.getString("introduce");
+                String achievement = rs.getString("achievement");
+                String avatar = rs.getString("avatar");
+                float costHire = rs.getFloat("costHire");
+                list.add(new Mentor(id, accountid, mentorname, address, phone, birthday, sex, introduce, achievement, avatar, costHire));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+     
     public List<Skill> getallskill() {
         List<Skill> list = new ArrayList<>();
         query = "SELECT * FROM skill";
