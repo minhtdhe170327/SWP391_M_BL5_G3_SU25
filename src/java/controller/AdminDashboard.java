9f92126a115fd0de5a82489fc438925b9e9d4dee
Nguyen HE170225 <nguyennhlhe170225@fpt.edu.vn>
2025-08-25 22:59:12 +0700

    update code

--- src/java/controller/AdminDashboard.java
+++ src/java/controller/AdminDashboard.java
@@ -28,6 +28,8 @@ public class AdminDashboard extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -45,6 +47,7 @@ public class AdminDashboard extends HttpServlet {
         }
     }
 
+    
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -78,6 +81,8 @@ public class AdminDashboard extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -89,6 +94,8 @@ public class AdminDashboard extends HttpServlet {
      *
      * @return a String containing servlet description
      */
+    
+    
     @Override
     public String getServletInfo() {
         return "Admin Dashboard Controller";
